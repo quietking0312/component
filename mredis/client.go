7e7db54f439c233c784dd77e8f49aa1b33b048de
quiet <ztwangjing@7doc.cn>
2023-03-14 14:49:48 +0800

    init

--- mredis/client.go
+++ mredis/client.go
@@ -1,22 +1,6 @@
 package mredis
 
-import (
-	"context"
-	"github.com/redis/go-redis/v9"
+const (
+	ModeCluster = "cluster"
+	ModeSingle  = "single"
 )
-
-type RedisClient interface {
-	Get(ctx context.Context, key string) *redis.StringCmd
-	GetRange(ctx context.Context, key string, start, end int64) *redis.StringCmd
-
-	Do(ctx context.Context, args ...interface{}) *redis.Cmd
-	Watch(ctx context.Context, fn func(tx *redis.Tx) error, keys ...string) error
-}
-
-func NewClient(a string) (RedisClient, error) {
-	if a == "" {
-		return NewRedisClient()
-	} else {
-		return NewRedisClusterClient()
-	}
-}
