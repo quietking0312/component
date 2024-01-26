05ab91f3f87b68b06cc6ab4bb1f0725eea9536dc
quiet.king <quiet_king0312@163.com>
2024-01-26 11:03:06 +0800

    update

--- mnet/tcp_client_test.go
+++ mnet/tcp_client_test.go
@@ -1,36 +1,42 @@
 package mnet
 
-//func Test_NEWTCPClient(t *testing.T) {
-//	cli := NewTCPClient("127.0.0.1:8888")
-//	conn := cli.Dial()
-//	tcpConn := newTCPConn("", conn, _log)
-//	var route = NewRouter()
-//	c := func(message proto.Message) string {
-//		msgType := reflect.TypeOf(message)
-//		return strconv.FormatInt(int64(pb.S2C_value[strings.ToLower(msgType.Elem().Name())]), 10)
-//	}
-//	route.Register(c(&pb.Pong{}), func(msg Msg, a AgentIface) {
-//		var req pb.Pong
-//		if err := proto.Unmarshal(msg.Data, &req); err != nil {
-//			t.Fatal(err)
-//		}
-//		fmt.Println(req.GetData())
-//	}, func(msg Msg, a AgentIface) {
-//		fmt.Println("3333")
-//	})
-//	ag := &Agent{
-//		conn:    tcpConn,
-//		log:     _log,
-//		parser:  &ProtoParser{},
-//		handler: route,
-//	}
-//	go func() {
-//		ag.Run()
-//	}()
-//	for {
-//		ag.Write(&pb.Ping{
-//			Args: "hello",
-//		})
-//		time.Sleep(5 * time.Second)
-//	}
-//}
+import (
+	"fmt"
+	"github.com/quietking0312/component/mnet/pb"
+	"google.golang.org/protobuf/proto"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+func Test_NEWTCPClient(t *testing.T) {
+	cli := NewTCPClient("127.0.0.1:8888")
+	conn := cli.Dial()
+	tcpConn := newTCPConn("", conn, _log)
+	var route = NewRouter()
+	c := func(message proto.Message) string {
+		msgType := reflect.TypeOf(message)
+		return strconv.FormatInt(int64(pb.S2C_value[strings.ToLower(msgType.Elem().Name())]), 10)
+	}
+	route.Register(c(&pb.Pong{}), func(c Context) {
+		var req pb.Pong
+		if err := proto.Unmarshal(c.GetMsg().Data, &req); err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(req.GetData())
+	}, func(c Context) {
+		fmt.Println("3333")
+	})
+	ag := NewAgent(tcpConn, &ProtoParser{}, route)
+	go func() {
+		ag.Run()
+	}()
+	for {
+		ag.Write(&pb.Ping{
+			Args: "hello",
+		})
+		time.Sleep(5 * time.Second)
+	}
+}
--- mnet/tcp_server_test.go
+++ mnet/tcp_server_test.go
@@ -1,30 +1,41 @@
 package mnet
 
-//func Test_NewTCPServer(t *testing.T) {
-//	ln, err := net.Listen("tcp", "0.0.0.0:8888")
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	var route = NewRouter()
-//	c := func(message proto.Message) string {
-//		msgType := reflect.TypeOf(message)
-//		return strconv.FormatInt(int64(pb.C2S_value[strings.ToLower(msgType.Elem().Name())]), 10)
-//	}
-//	route.Register(c(&pb.Ping{}), func(msg Msg, a AgentIface) {
-//		var req pb.Ping
-//		if err := proto.Unmarshal(msg.Data, &req); err != nil {
-//			t.Fatal(err)
-//		}
-//		fmt.Println(req.GetArgs())
-//		a.Write(&pb.Pong{
-//			Data: "go 1.20",
-//		})
-//	}, func(msg Msg, a AgentIface) {
-//		fmt.Println("2222")
-//	})
-//	ser := NewTCPServer(65535, func(conn *TCPConn) AgentIface {
-//		a := &Agent{conn: conn, log: _log, parser: &ProtoParser{}, handler: route}
-//		return a
-//	})
-//	ser.Serve(ln)
-//}
+import (
+	"fmt"
+	"github.com/quietking0312/component/mnet/pb"
+	"google.golang.org/protobuf/proto"
+	"net"
+	"reflect"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func Test_NewTCPServer(t *testing.T) {
+	ln, err := net.Listen("tcp", "0.0.0.0:8888")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var route = NewRouter()
+	c := func(message proto.Message) string {
+		msgType := reflect.TypeOf(message)
+		return strconv.FormatInt(int64(pb.C2S_value[strings.ToLower(msgType.Elem().Name())]), 10)
+	}
+	route.Register(c(&pb.Ping{}), func(c Context) {
+		var req pb.Ping
+		if err := proto.Unmarshal(c.GetMsg().Data, &req); err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(req.GetArgs())
+		c.Write(&pb.Pong{
+			Data: "go 1.20",
+		})
+	}, func(c Context) {
+		fmt.Println("2222")
+	})
+	ser := NewTCPServer(65535, func(conn *TCPConn) AgentIface {
+		a := NewAgent(conn, &ProtoParser{}, route)
+		return a
+	})
+	ser.Serve(ln)
+}
